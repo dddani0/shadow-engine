4c2a13906002e152892946d2463fbfadb7dd4a35
Devley0 <getkhun@gmail.com>
2026-04-09 09:30:09 +0200

    Added project collection page, with pagination

--- frontend/src/app/pages/project-collection-page/project-collection-page.ts
+++ frontend/src/app/pages/project-collection-page/project-collection-page.ts
@@ -1,19 +1,24 @@
 import { Component, inject, signal } from '@angular/core';
 import { ApiClient } from '../../api/api-client';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { Project } from '../../api/api-types';
 import { errorContext } from 'rxjs/internal/util/errorContext';
 
 @Component({
   selector: 'project-collection-page',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './project-collection-page.html',
 })
 export class ProjectCollectionPage {
   private readonly api = inject(ApiClient);
   private readonly route = inject(ActivatedRoute);
 
-  private readonly projects = signal<Project[] | null>(null);
+  readonly projects = signal<Project[] | null>(null);
+
+  allowedProjectsPerPage = 4;
+  readonly startIndexOfProjects = signal<number>(0);
+  readonly pages = signal<number>(0);
+  readonly page = signal<number>(0);
 
   constructor() {
     this.refresh();
@@ -23,8 +28,29 @@ export class ProjectCollectionPage {
     this.api.listProjects().subscribe({
       next: (p) => {
         this.projects.set(p);
+        this.pages.set(this.projects()?.length! % this.allowedProjectsPerPage);
+        console.log(this.pages());
       },
       error: (err) => console.log(err),
     });
+  }
+
+  deleteProject(id: string) {
+    if (!confirm('Delete this project?')) return;
+    this.api.deleteProject(id).subscribe({
+      next: () => this.refresh(),
+    });
+  }
+
+  setPage(index: number) {
+    this.page.set(index);
+  }
+
+  nextPage() {
+    this.page.set(this.page() + 1);
+  }
+
+  previousPage() {
+    this.page.set(this.page() - 1);
   }
 }
